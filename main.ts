8bf560a1a9e9c11c3ce951ba301a3369290f7c14
Dominik Kotecki <dominik.kotecki@rossmann.com.pl>
2021-04-09 15:53:47 +0200

    small changes

--- main.ts
+++ main.ts
@@ -1,10 +1,19 @@
 import { Construct } from 'constructs';
 import { App, TerraformStack } from 'cdktf';
+import {KubernetesProvider, Namespace, Deployment, Service} from './.gen/providers/kubernetes';
+
+function createDeployment(stack: TerraformStack, appName: string) {
+  const dep = new Deployment(stack, `${appName}-deployment`, {
+    metadata: [{ name: appName }]
+  })
+}
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
+    const ks8 = new KubernetesProvider(this, "test-k8s")
 
+    const app
     // define resources here
 
   }
